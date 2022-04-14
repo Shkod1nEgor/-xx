59f9135b8d257ceddfa20e5cbf9a1e612e5d752b
Егор Шкодин <shkodinu1@tk34.mskobr.ru>
2022-04-14 13:58:27 +0300

    Задание №3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,30 +5,13 @@ namespace ConsoleApp2
 {
     class Program
     {
-        private static double[] vector;
+        
 
         static void Main(string[] args)
         {
-           vector = new double[100];
-            var rand = new Random();
-            // заполняем массив случайными значениями
-            for (var i = 0; i < vector.Length; i++)
-                vector[i] = rand.NextDouble() * 100;
-            // находим случайный индекс
-            var indexForSqrt = rand.Next(vector.Length);
-            // находим квадратный корень
-            var sqrtValue = Math.Sqrt(vector[indexForSqrt]);
-            Console.WriteLine(@"Квадратный корень из vector[{0}]={1} равен: {2}",
-                                indexForSqrt, vector[indexForSqrt], sqrtValue);
-            // находим первый случайный индекс
-            var indexOneForAverage = rand.Next(vector.Length);
-            // находим второй случайный индекс
-            var indexTwoForAverage = rand.Next(vector.Length);
-            // находим усреднение
-            var averageValue = (vector[indexOneForAverage] + vector[indexTwoForAverage]) / 2;
-            Console.WriteLine(@"Среднее арифметическое vector[{0}]={1} и vector[{2}]={3} равно: {4}",
-             indexOneForAverage, vector[indexOneForAverage], indexTwoForAverage, vector[indexTwoForAverage], averageValue);
-
+            string[] result = "Предложение из десяти слов, с кучей пробелов   и запятыми, точками.".Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < result.Length; i++)
+                result[i] = result[i].Trim().Replace(",", String.Empty).Replace(".", String.Empty);
 
         }
     }   
